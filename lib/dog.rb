968b4d505dc8347203917b81c0fb511e699f1f1a
Maika Kailani <queenmab051@gmail.com>
2020-03-26 19:19:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,30 +1,31 @@
 class Dog
-  @all = []
-  
+  @@all = []
+
   def initialize(name)
     @name = name
     self.save
   end
-  
-  def name
-    @name
-  end
-  
+
   def self.all
-    @all
+    @@all
   end
-  
+
+  def self.clear_all
+    @@all = [] 
+  end
+
   def self.print_all
     @@all.each do |dog|
       puts dog.name
     end
-  end
-  
+end
+
   def save
     @@all << self
   end
-  
-  def self.clear_all
-    @all = []
+
+  def name
+    @name
   end
-end
+
+end 
